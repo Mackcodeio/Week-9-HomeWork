c14f8bea69bfd8d08225aab5ec0ed381391527e3
Mackcode <maheshhirpara.connect@gmail.com>
2023-05-26 14:52:18 +0100

    selenium homework

--- Week 9/src/browertesting1/MultiBrowserTesting.java
+++ Week 9/src/browertesting1/MultiBrowserTesting.java
@@ -1,4 +1,46 @@
 package browertesting1;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
 public class MultiBrowserTesting {
+
+    static String browser = "edge";
+
+    static WebDriver driver;
+
+    static String url = "https://demo.nopcommerce.com/";
+
+    static String expectedTitle = "nopCommerce demo store";
+    public static void main(String[] args) {
+        if(browser.equalsIgnoreCase("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+        }else if(browser.equalsIgnoreCase("firefox")){
+            FirefoxOptions options = new FirefoxOptions();
+            driver = new FirefoxDriver(options);
+        }else if(browser.equalsIgnoreCase("edge")){
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver();
+        }else {
+            System.out.println("Invalid Browser");
+        }
+
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4000));
+        String actualTitle = driver.getTitle();
+        System.out.println("Title of page is: "+actualTitle);
+
+        Assert.assertTrue(expectedTitle.equalsIgnoreCase(actualTitle));
+        driver.quit();
+    }
 }
--- Week 9/src/browertesting9/MultiBrowserTesting.java
+++ Week 9/src/browertesting9/MultiBrowserTesting.java
@@ -1,4 +1,42 @@
 package browertesting9;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.time.Duration;
+
 public class MultiBrowserTesting {
+    static String brower = "Chrome";
+    static WebDriver driver;
+    static String url ="https://login.paytmmoney.com/";
+
+    static String expectedTitle ="Login - Audi connect";
+    public static void main(String[] args) {
+        if (brower.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+        } else if (brower.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            driver = new FirefoxDriver(options);
+        } else if (brower.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver(options);
+        } else {
+            System.out.println("Invalid Brower");
+        }
+
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        String actuaTitle = driver.getTitle();
+        System.out.println("Title of page is: "+actuaTitle);
+        Assert.assertTrue(expectedTitle.equalsIgnoreCase(actuaTitle));
+        driver.quit();
+    }
 }
